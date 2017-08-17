d2e71fa437a914cd26d47021de2d83cfc1e52a19
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-08-17 18:01:26 +0100

    Using Headless Chrome (Puppeteer) to fetch QueryID.

--- test.js
+++ test.js
@@ -1,11 +1,38 @@
 var instagram = require('./index');
 
-/*
 // Initialization
-console.log(instagram.search.init(17875800862117404, 500, 360000));
+await instagram.init();
 
 // Get recent
-instagram.search.hashtag.recent('hpow', 1000);
-*/
+// instagram.search.hashtag.recent('hpow', 1000);
 
-console.log(instagram.tags.info());
+console.log(instagram.tags.info('hpow'));
+
+/*
+
+const
+    puppeteer = require('puppeteer'),
+    URL = require('url');
+
+(async () => {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setRequestInterceptionEnabled(true);
+    page.on('request', request => {
+        if (request.url.startsWith('https://www.instagram.com/graphql/query/')) {
+            console.log('Query ID:\t', request.url.split('=')[1].split('&')[0]);
+        } else {
+            request.continue();
+        }
+    });
+    await page.goto('https://www.instagram.com/explore/tags/instagram/');
+
+    // Click on 'more images'
+    await page.click('._1cr2e._epyes');
+
+    browser.close();
+
+})();
+
+*/
