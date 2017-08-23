1e2be5c077504af2e5d7ab1a9a3a09f04e84530a
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-08-23 16:16:21 +0100

    User info done and tested. User posts will be another method.

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var instagram = require('./index');
 
 async function go() {
-    var info = await instagram.tags.info('hpow');
-    console.log(info);
+    // var info = await instagram.tags.info('hpow');
+    // console.log(info);
 
     // const posts = await instagram.tags.recent('hpow', 1000);
     // console.log(posts.length);
 
-    const hashtags = await instagram.tags.search('canon');
-    console.log(hashtags);
+    // const hashtags = await instagram.tags.search('canon');
+    // console.log(hashtags);
 
-    const post = await instagram.media.get('BQYvbMmAgHz');
-    console.log(post);
+    // const post = await instagram.media.get('BQYvbMmAgHz');
+    // console.log(post);
+
+    const user = await instagram.users.info('heatonthomas');
+    console.log(user);
 };
 
 go();
