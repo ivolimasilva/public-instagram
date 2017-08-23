2047259bb608ad15999dde14da83a942b5a06325
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-08-23 17:13:32 +0100

    Changed some test subjects.

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var instagram = require('./index');
 
 async function go() {
-    // var info = await instagram.tags.info('hpow');
+
+    // var info = await instagram.tags.info('instagram');
     // console.log(info);
 
-    // const posts = await instagram.tags.recent('hpow', 1000);
+    // const posts = await instagram.tags.recent('instagram', 1000);
     // console.log(posts.length);
 
-    // const hashtags = await instagram.tags.search('canon');
+    // const hashtags = await instagram.tags.search('instagram');
     // console.log(hashtags);
 
-    // const post = await instagram.media.get('BQYvbMmAgHz');
+    // const post = await instagram.media.get('BP-rXUGBPJa');
     // console.log(post);
 
-    const user = await instagram.users.info('heatonthomas');
+    const user = await instagram.users.info('instagram');
     console.log(user);
+
 };
 
 go();
